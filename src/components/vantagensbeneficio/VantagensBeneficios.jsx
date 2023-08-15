63d64d45b3ed3bc7632aeca6dab46467e40d0818
XOFAA <giovanne.oliv@gmail.com>
2023-08-15 10:23:52 -0400

    vantagens e beneficio alteracao

--- src/components/vantagensbeneficio/VantagensBeneficios.jsx
+++ src/components/vantagensbeneficio/VantagensBeneficios.jsx
@@ -12,7 +12,9 @@ export const VantagensBeneficio = () => {
      <Container>
         <Typography sx={{textAlign:'center',fontSize:'28px',fontStyle:'italic',pt:5}}><span style={{fontWeight:'bold',fontStyle:'italic'}}>Vantagens e Benefícios</span> que oferecemos à sua disposição</Typography>
     <Box sx={{display:'flex'}}>
-
+    <Box>
+    
+    </Box>
     </Box>
      </Container>
     </Box>
