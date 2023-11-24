f816d21c7016e9ceef4b2f7aeb8319a824cf15e5
xofaa <giovanne.oliv@gmail.com>
2023-11-24 08:38:15 -0400

    correcao funcao appwithpreloader

--- src/components/appwithpreloader/AppWithPreloader.jsx
+++ src/components/appwithpreloader/AppWithPreloader.jsx
@@ -1,10 +1,10 @@
 import React, { useEffect, useState } from "react";
 import Preloader from "../preloader/Preloader";
 
-
 function AppWithPreloader({ children }) {
   const [loadedImages, setLoadedImages] = useState(0);
   const [totalImages, setTotalImages] = useState(0);
+  const [errorImages, setErrorImages] = useState(0);
 
   useEffect(() => {
     const images = document.querySelectorAll("img");
@@ -14,22 +14,30 @@ function AppWithPreloader({ children }) {
       setLoadedImages((prevLoadedImages) => prevLoadedImages + 1);
     };
 
+    const handleImageError = () => {
+      setErrorImages((prevErrorImages) => prevErrorImages + 1);
+    };
+
     images.forEach((image) => {
       if (image.complete) {
         handleImageLoad();
       } else {
         image.addEventListener("load", handleImageLoad);
+        image.addEventListener("error", handleImageError);
       }
     });
 
     return () => {
       images.forEach((image) => {
         image.removeEventListener("load", handleImageLoad);
+        image.removeEventListener("error", handleImageError);
       });
     };
   }, []);
 
-  return loadedImages < totalImages ? <Preloader /> : children;
+  const allImagesLoaded = loadedImages + errorImages === totalImages;
+
+  return allImagesLoaded ? children : <Preloader />;
 }
 
 export default AppWithPreloader;
